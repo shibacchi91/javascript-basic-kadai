100dbc2419d2736d567d19fdaeb9673b921f4a86
shibacchi91 <153363821+shibacchi91@users.noreply.github.com>
2024-02-02 22:31:49 +0900

    kadai_020を追加

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+// DOM要素とDOM操作
+
+const jsBtn = document.getElementById('btn');
+jsBtn.addEventListener('click', () => {
+    const jsText = document.getElementById('text');
+    jsText.textContent = 'ボタンをクリックしました'
+});
+
+
+
